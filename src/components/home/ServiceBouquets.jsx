4f95b39c682d48e881e913dd7efdcb37827f9b9b
ejidike esther <ejidikeesther@gmail.com>
2021-12-23 12:37:55 +0100

    Add Service Bouquet Section

--- src/components/home/ServiceBouquets.jsx
+++ src/components/home/ServiceBouquets.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Medical from '../../assets/images/medical-check.jpg'
+import Background from '../../assets/images/background-check.jpg'
+import Icons from '../reusable/Icons'
 
 function ServiceBouquets() {
     return (
@@ -8,9 +12,35 @@ function ServiceBouquets() {
                 <div className='py-5 mt-5 text-center'>
                     <h2 className='fancy-font'>Service Bouquets</h2>
                     <h3>Select your preferred service bouquet</h3>
-                    <Row>
-                        <Col xs={12}></Col>
-                        <Col xs={12}></Col>
+                    <Row className='justify-content-md-center mt-4'>
+                        <Col xs={12} sm={6} lg={5}>
+                            <div className='medicals-image-container mx-auto'>
+                                <img src={Medical} className='w-100 h-100 object-fit-cover object-position-top' alt="smiling-lady" />
+                            </div>
+                            <div className='mt-3'>
+                                <h5>Medical Check</h5>
+                                <p className='mt-2'>
+                                    <Link to='#'>
+                                        Select option
+                                        <Icons icon='chevrons-right'/>
+                                    </Link>
+                                </p>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={6} lg={5}>
+                            <div className='medicals-image-container mx-auto'>
+                                <img src={Background} className='w-100 h-100 object-fit-cover object-position-top' alt="smiling-lady" />
+                            </div>
+                            <div className='mt-3'>
+                                <h5>Background & Criminal Check</h5>
+                                <p className='mt-2'>
+                                    <Link to='#'>
+                                        Select option
+                                        <Icons icon='chevrons-right'/>
+                                    </Link>
+                                </p>
+                            </div>
+                        </Col>
                     </Row>
                 </div>
             </Container>
